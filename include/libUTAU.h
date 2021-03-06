3c6d8a0f99481398bb42e82dace9829e9d77f554
YuzukiTsuru <gloomyghost@gloomyghost.com>
2021-03-07 00:34:07 +0800

    clean the code, My boss forced me to build this feature... Pure shit.

---
+++ include/libUTAU.h
@@ -0,0 +1,8 @@
+//
+// Created by YuzukiTsuru on 2021/3/7.
+//
+
+#ifndef LIBUTAU_LIBUTAU_H
+#define LIBUTAU_LIBUTAU_H
+
+#endif //LIBUTAU_LIBUTAU_H
---
+++ src/libUTAU.cpp
@@ -0,0 +1,4 @@
+//
+// Created by YuzukiTsuru on 2021/3/7.
+//
+
